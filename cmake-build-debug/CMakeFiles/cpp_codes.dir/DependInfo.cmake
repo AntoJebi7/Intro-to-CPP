fdf735e69e347c96782c1f357d7c08a68b59443c
AntoJebi7 <dantojebi@gmail.com>
2024-06-26 16:09:26 +0530

    testing

---
+++ cmake-build-debug/CMakeFiles/cpp_codes.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/cpp_codes/My Leetcode/palindrome number.cpp" "CMakeFiles/cpp_codes.dir/My_Leetcode/palindrome_number.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/My_Leetcode/palindrome_number.cpp.obj.d"
+  "D:/cpp_codes/My Leetcode/roman to integer.cpp" "CMakeFiles/cpp_codes.dir/My_Leetcode/roman_to_integer.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/My_Leetcode/roman_to_integer.cpp.obj.d"
+  "D:/cpp_codes/cppcontrolflow.cpp" "CMakeFiles/cpp_codes.dir/cppcontrolflow.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cppcontrolflow.cpp.obj.d"
+  "D:/cpp_codes/cmake-build-debug/cppdtypes.cpp" "CMakeFiles/cpp_codes.dir/cppdtypes.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cppdtypes.cpp.obj.d"
+  "D:/cpp_codes/cppenum.cpp" "CMakeFiles/cpp_codes.dir/cppenum.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cppenum.cpp.obj.d"
+  "D:/cpp_codes/cpploops.cpp" "CMakeFiles/cpp_codes.dir/cpploops.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cpploops.cpp.obj.d"
+  "D:/cpp_codes/cpppaterns.cpp" "CMakeFiles/cpp_codes.dir/cpppaterns.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cpppaterns.cpp.obj.d"
+  "D:/cpp_codes/cpppyramid.cpp" "CMakeFiles/cpp_codes.dir/cpppyramid.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cpppyramid.cpp.obj.d"
+  "D:/cpp_codes/cppsamples.cpp" "CMakeFiles/cpp_codes.dir/cppsamples.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cppsamples.cpp.obj.d"
+  "D:/cpp_codes/cppstructure.cpp" "CMakeFiles/cpp_codes.dir/cppstructure.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/cppstructure.cpp.obj.d"
+  "D:/cpp_codes/diamond pattern.cpp" "CMakeFiles/cpp_codes.dir/diamond_pattern.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/diamond_pattern.cpp.obj.d"
+  "D:/cpp_codes/inverted pyramid.cpp" "CMakeFiles/cpp_codes.dir/inverted_pyramid.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/inverted_pyramid.cpp.obj.d"
+  "D:/cpp_codes/main.cpp" "CMakeFiles/cpp_codes.dir/main.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/main.cpp.obj.d"
+  "D:/cpp_codes/mirror image pattern.cpp" "CMakeFiles/cpp_codes.dir/mirror_image_pattern.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/mirror_image_pattern.cpp.obj.d"
+  "D:/cpp_codes/pyramid using 2 for loops.cpp" "CMakeFiles/cpp_codes.dir/pyramid_using_2_for_loops.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/pyramid_using_2_for_loops.cpp.obj.d"
+  "D:/cpp_codes/right to left half pyramid.cpp" "CMakeFiles/cpp_codes.dir/right_to_left_half_pyramid.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/right_to_left_half_pyramid.cpp.obj.d"
+  "D:/cpp_codes/up arrow pattern.cpp" "CMakeFiles/cpp_codes.dir/up_arrow_pattern.cpp.obj" "gcc" "CMakeFiles/cpp_codes.dir/up_arrow_pattern.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cpp_codes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp_codes.
+# This may be replaced when dependencies are built.
