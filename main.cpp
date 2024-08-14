3d9e118b9582b02434a89bda652d67834772b7af
AntoJebi7 <dantojebi@gmail.com>
2024-08-14 14:59:51 +0530

    modified

--- main.cpp
+++ main.cpp
@@ -1,7 +1,176 @@
-#include <iostream>
+#include <cstring>
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cstring>
+#include<fstream>
+using namespace std;
+/*
+ *
+* struct anto {
+    long int number;
+    string name;
+};
+enum food {
+    juice,
+    shakes,
+    fastfoods
+};
+
+
+int sum(int a , int b) {
+    return a+b;
+}
+int multiply(int *x , int *y) {
+    int temp = *x;
+    *x = *y;
+    *y= temp;
+    cout<<"value of a is "<<*x<<endl;
+    cout<<"value of b is "<<*y<<endl;
+    return 0;
+
+
+}
+
+int factorial(int n){
+    if(n==0 || n==1) {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+    std::cout<<"hello world";
+    float PI = 3.14;
+    int num = 100;
+    cout << "Entering a new line." << endl;
+    cout << setw(10) << "Output" << endl;
+    cout << setprecision(10) << PI << endl;
+    cout << setbase(16) << num << endl;
+    int a[]={10,324,13,34};
+    int *ptr = a;
+    cout<<*ptr<<endl;
+    int arrs=sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<arrs;i++) {
+        cout<<*(ptr+i)<<endl;
+
+    }
+    struct anto an;
+    an.name ="anto jebikshan";
+    cout<<an.name<<endl;
+    food f1 = fastfoods;
+    if (f1==2) {
+        cout<<f1;
+    }
+    int a=10,b=78;
+    cout<<"value of a is "<<a<<endl;
+    cout<<"value of b is "<<b<<endl;
+    cout<<multiply(&a,&b);
+
+    string name = "anto";
+    cout<<name;
+    int a=10;
+    cout<<a<<endl;
+    int *b=&a;
+    int **c=&b;
+    cout<<b<<endl;
+    cout<<factorial(5);
+class anto {
+    static int age ;
+public:
+    void printage() {
+        cout<<"your age is : "<<age<<endl;
+    }
+    static int getage() {
+        return age;
+    }
+    anto() {
+        age=90;
+    }
+
+};
+int anto::age=34;
+anto aj;
+    aj.printage();
+    cout<<anto::getage()<<endl;
+
+class dog {
+public:
+    void dogo() {
+        cout<<"dog";
+
+    }
+};
+class cat:public virtual dog {
+public:
+    void cato() {
+        cout<<"cat";
+        dogo();
+    }
+
+};
+class rat {
+public:
+    void rato() {
+        cout<<"rat";
+
+    }
+
+};
+class bat:public cat,rat {
+public:
+    void bato() {
+        cout<<"bat";
+        dogo();
+        cato();
+        rato();
+
+    }
+
+};
+
+string str="Anto Jebikshan";
+    ofstream out("anto.txt");
+    out<< str;
+
+    out.close();
+
+
+*/
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "" << std::endl;
+
     return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
